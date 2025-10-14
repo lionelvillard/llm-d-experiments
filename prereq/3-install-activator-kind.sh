f08d9b787b25bb9c3a9b31af119c4758be80d16a
Lionel Villard <villard@us.ibm.com>
2025-10-14 16:04:40 -0400

    add activator installation script

---
+++ prereq/3-install-activator-kind.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source $(dirname $0)/utils.sh
+
+TMP_DIR=$(mktemp -d)
+
+git clone https://github.com/dumb0002/llm-d-activator.git ${TMP_DIR}
+log_success "Cloned llm-d-activator repository."
+
+pushd ${TMP_DIR} > /dev/null
+
+make image-kind KIND_CLUSTER=llm-d STAGING_IMAGE_REGISTRY=kind.local GIT_TAG=dev
+log_success "Activator image built and loaded."
+
+cd charts
+helm install activator-filter ./activator-filter --set name=activator
+
+popd # TMP_DIR
